c98ad482123f4c102606021f3c1a10a200c7bdee
Will <literallythevoid@protonmail.com>
2021-11-06 15:34:32 -0400

    Add recursive solution

--- problems/problem_2/problem_2_recursive.rb
+++ problems/problem_2/problem_2_recursive.rb
@@ -2,15 +2,37 @@
 
 require '../problem_base'
 
-# https://projecteuler.net/problem=1
+# https://projecteuler.net/problem=2
 class ProblemTwoRecursive < ProblemBase
-  # def self.result
-    
-  # end
+  MAX_VALUE = 4_000_000
+
+  def self.result
+    n = 0
+    sum = 0
+
+    while fibonacci(n) < MAX_VALUE
+      sum += fibonacci(n) if fibonacci(n).even?
+      n += 1
+    end
+
+    sum
+  end
+
+  def self.fibonacci(n)
+    return n if (0..1).include?(n)
+
+    (fibonacci(n - 1) + fibonacci(n - 2))
+  end
 end
 
 describe ProblemTwoRecursive, '.result' do
-  it 'returns 4613732' do
-    expect(ProblemTwoRecursive.result).to eq(4613732)
+  it 'returns 4,613,732' do
+    expect(ProblemTwoRecursive.result).to eq(4_613_732)
+  end
+end
+
+describe ProblemTwoRecursive, '.benchmark' do
+  it 'returns 4,613,732' do
+    expect(ProblemTwoRecursive.benchmark).to be < 10
   end
 end
